9e3e58711d6c07a0bbb8ef7dab1ab308be330094
jvfnjnwnr <73676681+jvfnjnwnr@users.noreply.github.com>
2022-10-18 14:06:01 +1000

    Update sensor_app.py

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -9,7 +9,7 @@ print("Sensor Data App")
 ##############################
 
 # Module 1 code here:
-data = load_sensor_data
+data = load_sensor_data()
 print("Loaded records: {}".format(len(data)))
 # Module 2 code here:
 
